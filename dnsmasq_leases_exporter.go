9a0fd91dbf96e6c4e7f76ab8995246d830a58470
Andreas Groß <mail@kwijybo.de>
2021-06-21 02:09:54 +0200

    Added text parser and json encoder; tests green

--- dnsmasq_leases_exporter.go
+++ dnsmasq_leases_exporter.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -16,16 +22,83 @@ var (
 )
 
 type Lease struct {
-	expiryTime int
-	macAddress string
-	ip         string
-	hostname   string
-	clientID   string
+	ExpiryTime int    `json:"expiryTime"`
+	MacAddress string `json:"macAddress"`
+	Ip         string `json:"ip"`
+	Hostname   string `json:"hostname"`
+	ClientID   string `json:"clientID"`
 }
 
-func parseLeasesFile(fileUrl *string) []Lease {
+func leaseFromText(text string) (*Lease, error) {
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	scanner.Split(bufio.ScanWords)
+
+	t := ""
+	l := Lease{}
+	column := 0
+
+	for scanner.Scan() {
+		t = scanner.Text()
+
+		// Count column
+		column += 1
+		switch {
+		case column == 1:
+			i, err := strconv.Atoi(t)
+			if err != nil {
+				return &l, errors.New(fmt.Sprintf("Could not parse %s as expiry time\n", t))
+			}
+			l.ExpiryTime = i
+		case column == 2:
+			l.MacAddress = t
+		case column == 3:
+			l.Ip = t
+		case column == 4:
+			l.Hostname = t
+		case column == 5:
+			l.ClientID = t
+
+		}
+	}
+
+	if column != 5 {
+
+		return &l, errors.New(fmt.Sprintf("Unexpected number of columns in leases file, expected 5, got %d\n", column))
+	}
+
+	return &l, nil
+}
+
+func parseLeasesFile(fileUrl *string) ([]Lease, error) {
 	var leases []Lease
-	return leases
+	f, err := os.Open(*fileUrl)
+
+	if err != nil {
+		// File could not be read; prit error to output and return a nil
+
+		return leases, errors.New(fmt.Sprintf("Could not read file %s: %s\n", *fileUrl, err))
+	}
+
+	// Close file at the end
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		l, err := leaseFromText(scanner.Text())
+		if err == nil {
+			leases = append(leases, *l)
+		}
+	}
+
+	return leases, nil
+}
+
+func encodeLeasesToJson(leases []Lease) (string, error) {
+	b, err := json.Marshal(leases)
+	if err == nil {
+		return string(b), nil
+	}
+	return "", err
 }
 
 func main() {
--- dnsmasq_leases_exporter_test.go
+++ dnsmasq_leases_exporter_test.go
@@ -4,11 +4,79 @@ import (
 	"testing"
 )
 
+func TestLeaseFromText(t *testing.T) {
+	// Test if string gets converter correctly
+	var text1 = "1623926767 10:10:30:40:50:60 10.10.1.2 some-host 01:10:10:30:40:50:60"
+	var text2 = "1623926767 10:10:30:40:50:60 10.10.1.2 some-host 01:10:10:30:40:50:60 some extra junk"
+	var text3 = "too few columns"
+	var lease1, err1 = leaseFromText(text1)
+	var _, err2 = leaseFromText(text2)
+	var _, err3 = leaseFromText(text3)
+
+	if err1 != nil {
+		t.Errorf("Correct text not pared correctly")
+	}
+
+	if lease1.ExpiryTime != 1623926767 {
+		t.Errorf("Expiry time not pared correctly, expected: %d, got %d", 1623926767, lease1.ExpiryTime)
+	}
+	if lease1.MacAddress != "10:10:30:40:50:60" {
+		t.Errorf("MAC-Address not pared correctly, expected: %s, got %s", "10:10:30:40:50:60", lease1.MacAddress)
+	}
+	if lease1.Ip != "10.10.1.2" {
+		t.Errorf("IP not pared correctly, expected: %s, got %s", "10.10.1.2", lease1.Ip)
+	}
+	if lease1.Hostname != "some-host" {
+		t.Errorf("Hostname not pared correctly, expected: %s, got %s", "some-host", lease1.Hostname)
+	}
+	if lease1.ClientID != "01:10:10:30:40:50:60" {
+		t.Errorf("Client ID not pared correctly, expected: %s, got %s", "01:10:10:30:40:50:60", lease1.ClientID)
+	}
+
+	if err2 == nil {
+		t.Errorf("Text with extra columns parsed, expected nil")
+	}
+
+	if err3 == nil {
+		t.Errorf("Text with too few columns parsed, expected nil")
+	}
+}
+
 func TestFileParser(t *testing.T) {
 	//
 	var fileUrl = "test/dnsmasq.leases"
-	var leases = parseLeasesFile(&fileUrl)
+	var leases, err = parseLeasesFile(&fileUrl)
+
+	if err != nil {
+		t.Errorf("Could not read test file.")
+	}
+
 	if len(leases) != 2 {
 		t.Errorf("number of parsed leases is %d, expected 2", len(leases))
+	}
+
+	if leases[0].ExpiryTime != 1623926766 {
+		t.Errorf("Expiry time of first test record not pared correctly, expected: %d, got %d", 1623926766, leases[0].ExpiryTime)
+	}
+
+	if leases[1].Hostname != "some-host" {
+		t.Errorf("Hostname of first test record not pared correctly, expected: %s, got %s", "some-host", leases[1].Hostname)
+	}
+}
+
+func TestFileParserWithNonExistingFile(t *testing.T) {
+	//
+	var fileUrl = "test/no/such/file"
+	var _, err = parseLeasesFile(&fileUrl)
+	if err == nil {
+		t.Errorf("Reading non existent file should return error")
+	}
+}
+
+func TestEncodeLeasesToJson(t *testing.T) {
+	leases := make([]Lease, 2)
+	_, err := encodeLeasesToJson(leases)
+	if err != nil {
+		t.Errorf("Could not encode slice of leases to json")
 	}
 }
